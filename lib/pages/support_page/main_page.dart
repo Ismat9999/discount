f45295d306347eee8cbde2fd768de6d5d9ba6f84
Ismat9999 <redgoo123@gmail.com>
2025-06-16 19:36:18 +0500

    main impl

--- lib/pages/support_page/main_page.dart
+++ lib/pages/support_page/main_page.dart
@@ -9,27 +9,138 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool isLoading= false;
   final List<Map<String, dynamic>> topCategories = [
-    {'title': 'Жильё для поездки', 'icon': Icons.backpack, 'page': TopPage(title: 'Жильё для поездки')},
-    {'title': 'Молл', 'icon': Icons.shopping_bag, 'page': TopPage(title: 'Молл')},
+    {
+      'title': 'Жильё для поездки',
+      'icon': Icons.backpack,
+      'page': TopPage(title: 'Жильё для поездки')
+    },
+    {
+      'title': 'Молл',
+      'icon': Icons.shopping_bag,
+      'page': TopPage(title: 'Молл')
+    },
     {'title': 'Гаражи', 'icon': Icons.garage, 'page': TopPage(title: 'Гаражи')},
     {'title': 'Работа', 'icon': Icons.work, 'page': TopPage(title: 'Работа')},
-    {'title': 'Услуги', 'icon': Icons.handyman, 'page': TopPage(title: 'Услуги')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
-    {'title': 'Электроника', 'icon': Icons.smartphone, 'page': TopPage(title: 'Электроника')},
+    {
+      'title': 'Услуги',
+      'icon': Icons.handyman,
+      'page': TopPage(title: 'Услуги')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
+    {
+      'title': 'Электроника',
+      'icon': Icons.smartphone,
+      'page': TopPage(title: 'Электроника')
+    },
   ];
 
   final List<Map<String, dynamic>> bottomCategories = [
-    {'title': 'Авто', 'image': 'assets/auto.png', 'page': BottomPage(title: 'Авто')},
-    {'title': 'Недвижи-\nмость', 'image': 'assets/realestate.png', 'page': BottomPage(title: 'Недвижимость')},
-    {'title': 'Услуги', 'image': 'assets/services.png', 'page': BottomPage(title: 'Услуги')},
-    {'title': 'Электро-\nника', 'image': 'assets/electro.png', 'page': BottomPage(title: 'Электроника')},
-    {'title': 'Для дома\nи дачи', 'image': 'assets/home.png', 'page': BottomPage(title: 'Для дома и дачи')},
+    {
+      'title': 'Авто',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Авто')
+    },
+    {
+      'title': 'Недвижи-\nмость',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Недвижимость')
+    },
+    {
+      'title': 'Услуги',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Услуги')
+    },
+    {
+      'title': 'Электро-\nника',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Электроника')
+    },
+    {
+      'title': 'Для дома\nи дачи',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Для дома и дачи')
+    },
+    {
+      'title': 'Авто',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Авто')
+    },
+    {
+      'title': 'Недвижи-\nмость',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Недвижимость')
+    },
+    {
+      'title': 'Услуги',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Услуги')
+    },
+    {
+      'title': 'Электро-\nника',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Электроника')
+    },
+    {
+      'title': 'Для дома\nи дачи',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Для дома и дачи')
+    },
+    {
+      'title': 'Авто',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Авто')
+    },
+    {
+      'title': 'Недвижи-\nмость',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Недвижимость')
+    },
+    {
+      'title': 'Услуги',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Услуги')
+    },
+    {
+      'title': 'Электро-\nника',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Электроника')
+    },
+    {
+      'title': 'Для дома\nи дачи',
+      'image': 'assets/images/logo1.png',
+      'page': BottomPage(title: 'Для дома и дачи')
+    },
   ];
 
   @override
@@ -44,30 +155,119 @@ class _MainPageState extends State<MainPage> {
               height: 50,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: topCategories.map((item){
+                children: topCategories.map((item) {
                   return GestureDetector(
-                    onTap: (){
-                      Navigator.push(context,MaterialPageRoute(builder: (_)=> item['page']),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => item['page']),
                       );
                     },
                     child: Row(
-                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Icon(item['icon'],size: 18,),
-                        SizedBox(width: 5,),
-                        Text(item['title'],style: TextStyle(fontSize: 13),),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(
+                          item['icon'],
+                          size: 18,
+                        ),
+                        Text(
+                          item['title'],
+                          style: TextStyle(fontSize: 13),
+                        ),
                       ],
                     ),
                   );
                 }).toList(),
               ),
             ),
+            Container(
+              width:(5 + 5) * 50.0,
+              height: 150,
+              child: GridView.builder(
+                itemCount: bottomCategories.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 5,mainAxisSpacing: 5,childAspectRatio: 1.2,),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  var item=bottomCategories[index];
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (_)=> item['page']));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey.shade300,
+                      ),
+                      child: Column(
+                        children: [
+                          Text(item['title'],textAlign:TextAlign.center,style: TextStyle(fontSize: 10),),
+                          SizedBox(height: 8,),
+                          Expanded(child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Image.asset(item['image'],fit: BoxFit.contain,height: 30,),
+                            ],
+                          )),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 500,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Expanded(
+                        child: Container(
+                          height: 200,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-        ),
-      );
+      ),
+    );
   }
 }
+
 class BottomPage extends StatelessWidget {
   final String title;
 
@@ -81,16 +281,17 @@ class BottomPage extends StatelessWidget {
     );
   }
 }
-  class TopPage extends StatelessWidget {
-    final String title;
 
-    TopPage({required this.title});
+class TopPage extends StatelessWidget {
+  final String title;
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: Text(title)),
-        body: Center(child: Text("Bu sahifa: $title")),
-      );
-    }
+  TopPage({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(child: Text("Bu sahifa: $title")),
+    );
   }
+}
